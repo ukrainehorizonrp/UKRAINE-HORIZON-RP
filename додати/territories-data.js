67bb763d6ca6cefa0e62c7cc47794d96e87bfd24
sqw1zy228 <sqw1zyxbro@gmail.com>
2026-09-26 14:51:49 +0300

    Create territories-data.js

---
+++ додати/territories-data.js
@@ -0,0 +1,20 @@
+const territoriesData = [
+    {
+        id: "terr_1",
+        title: "Центральний Бізнес-Центр",
+        image: "https://i.postimg.cc/MGKQnKVH/file-0000000025e481f68637ad281142736f.webp",
+        owner: "@nickname_owner",
+        startDate: "26.09.2026",
+        endDate: "31.09.2026",
+        status: "Арендовано"
+    },
+    {
+        id: "terr_2",
+        title: "Парковка біля ДСНС",
+        image: "https://i.postimg.cc/MGKQnKVH/file-0000000025e481f68637ad281142736f.webp",
+        owner: "Вільна",
+        startDate: "-",
+        endDate: "-",
+        status: "Вільна"
+    }
+];
